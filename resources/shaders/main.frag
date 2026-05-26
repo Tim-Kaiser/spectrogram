e21f3157435f8f8200631a6d0782be4e599753c9
Siiiiinth <60766886+Siiiiinth@users.noreply.github.com>
2026-05-27 01:04:03 +0200

    compute shader and texture setup

--- resources/shaders/main.frag
+++ resources/shaders/main.frag
@@ -1,10 +1,6 @@
 #version 460
 
-layout(std430, binding=0) buffer audioData
-{
-	float samples[];
-};
-uniform int u_time;
+uniform sampler2D u_spectrogramTex;
 
 in vec2 vUv;
 in vec3 vertexOut;
@@ -13,5 +9,5 @@ in vec3 vertexOut;
 out vec4 fragCol;
 
 void main(){
-	fragCol = vec4(vUv, 0., 1.);
+	fragCol = texture(u_spectrogramTex, vUv);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
 #include "../include/rreav_includes.h"
 #include "SFML/System/Clock.hpp"
+#include <iostream>
+#include <memory>
+
+void createTexture(GLuint *textureID, int width, int height) {
+  glGenTextures(1, textureID);
+  glActiveTexture(GL_TEXTURE2);
+  glBindTexture(GL_TEXTURE_2D, *textureID);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, width, height, 0, GL_RGBA,
+               GL_FLOAT, NULL);
+}
 
 int main() {
   //===== INIT =====
@@ -8,13 +20,18 @@ int main() {
   cfg->init("resources/config.json");
   Window window(cfg->getWindowName(), 800, 600);
   ShaderManager shaderManager;
-  AudioManager audioManager("resources/audio/sine_wave_1000hz_44.1sr.wav");
+  AudioManager audioManager("resources/audio/get_up.flac");
 
   std::unique_ptr<Shader> renderShader = shaderManager.CreateShaders(
       "resources/shaders/main.vert", "resources/shaders/main.frag");
-  glUseProgram(renderShader->m_shaderProgramID);
+
+  std::unique_ptr<ComputeShader> computeShader =
+      shaderManager.CreateComputeShader("resources/shaders/spectrogram.comp");
 
   Mesh mesh = loadObject("resources/objects/quad.obj");
+
+  GLuint spectrogramTextureID;
+  createTexture(&spectrogramTextureID, window.getWidth(), window.getHeight());
 
   audioManager.setVolume(0.2f);
   audioManager.play();
@@ -22,10 +39,35 @@ int main() {
 
   sf::Clock clock;
   clock.start();
+
   while (window.running()) {
+    glClear(GL_COLOR_BUFFER_BIT);
     int t = clock.getElapsedTime().asMilliseconds();
-    shaderManager.SendUniformData("u_time", t);
+
+    // Compute shader dispatch
+    glUseProgram(computeShader->m_shaderProgramID);
     audioManager.update();
+
+    // without this, SendUniformData reads width and height as
+    // glUniformMatrix4fv, fixed in newer RREAV version
+    int width, height;
+    width = window.getWidth();
+    height = window.getHeight();
+    shaderManager.SendUniformData("u_width", width);
+    shaderManager.SendUniformData("u_height", height);
+    shaderManager.SendUniformData("u_time", t);
+    glBindImageTexture(2, spectrogramTextureID, 0, GL_FALSE, 0, GL_READ_WRITE,
+                       GL_RGBA32F);
+    glDispatchCompute((GLuint)width, (GLuint)height, 1);
+    glMemoryBarrier(GL_ALL_BARRIER_BITS);
+
+    // Render shader
+    GLint texID = 0;
+
+    glUseProgram(renderShader->m_shaderProgramID);
+    glActiveTexture(GL_TEXTURE0);
+    shaderManager.SendUniformData("u_spectrogramTex", texID);
+    glBindTexture(GL_TEXTURE_2D, spectrogramTextureID);
 
     mesh.render();
     window.update();
